e6d8b135b12242aa673402399abe5e940f22c827
Jille <jillevandertogt@gmail.com>
2021-04-16 12:47:40 +0200

    Completed day 2

--- 2019/Day 2/day2.py
+++ 2019/Day 2/day2.py
@@ -3,35 +3,24 @@ import numpy as np
 
 def read_code(full_code):
 	def read_intcode(code, full_code):
-		#addition
-		if code[0] == 1:
-			full_code[code[3]] = full_code[code[1]] + full_code[code[2]]
-
-		#multiplication
-		elif code[0] == 2:
-			full_code[code[3]] = full_code[code[1]] * full_code[code[2]]
-
-		#completion
-		elif code[0] == 99:
-			return True
+		if code[0] == 1: full_code[code[3]] = full_code[code[1]] + full_code[code[2]]
+		elif code[0] == 2: full_code[code[3]] = full_code[code[1]] * full_code[code[2]]
+		elif code[0] == 99: return True
 		return False
 
 	for i in range(0, len(full_code), 4):
-		code = full_code[i:i+4]
-		if read_intcode(code, full_code):
+		if read_intcode(full_code[i:i+4], full_code):
 			break
 	return full_code
 
-def set_noun_verb(full_code, noun, verb):
-	full_code[1] = noun
-	full_code[2] = verb
-	return full_code
+def set_noun_verb(code, noun, verb):
+	code[1:3] = [noun, verb]
+	return code
 
 def find_value(full_code, value):
 	for noun in range(0,100):
 		for verb in range(0,100):
-			full_code = set_noun_verb(full_code, noun, verb)
-			if read_code(full_code.copy())[0] == value:
+			if read_code(set_noun_verb(full_code.copy(), noun, verb))[0] == value:
 				return noun, verb
 
 with open('day2.txt') as file:
