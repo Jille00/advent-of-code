a7a83ee2697b88fa30fc3092a40e2f42c2788c9b
Jille <37669698+Jille00@users.noreply.github.com>
2021-04-23 12:35:10 +0200

    Update day 8

--- 2019/Day 8/day8.py
+++ 2019/Day 8/day8.py
@@ -1,5 +1,4 @@
 import numpy as np
-from collections import Counter
 import matplotlib.pyplot as plt
 
 def find_next_non_transparant(layers, index):
@@ -10,17 +9,12 @@ def find_next_non_transparant(layers, index):
 with open('day8.txt') as file:
     total_image = np.array([i for i in file.read()])
 
-WIDTH = 25
-HEIGHT = 6
+WIDTH, HEIGHT = 25, 6
 layers = np.array(np.array_split(total_image, len(total_image)/(WIDTH*HEIGHT))).astype('int')
 zeros = np.count_nonzero(layers==0, axis=1)
 correct_layer = layers[np.argmin(zeros)]
 print("Part 1 is: ", np.count_nonzero(correct_layer==1)* np.count_nonzero(correct_layer==2))
 
-main_layer = layers[0]
-for index, i in enumerate(main_layer):
-	if i == 2:
-		main_layer[index] = find_next_non_transparant(layers, index)
-
-plt.imshow(main_layer.reshape((HEIGHT, WIDTH)), cmap='gray')
+main_layer = [find_next_non_transparant(layers, index) if i == 2 else i for index, i in enumerate(layers[0])]
+plt.imshow(np.array(main_layer).reshape((HEIGHT, WIDTH)), cmap='gray')
 plt.show()
